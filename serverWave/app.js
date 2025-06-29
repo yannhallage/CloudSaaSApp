ade8c5126ee94f424bf60f15e00874eb286cc575
thisisyannhallage <contact.devhllg@gmail.com>
2025-06-29 04:36:57 +0000

    Update app.js

--- serverWave/app.js
+++ serverWave/app.js
@@ -8,10 +8,11 @@ const cors = require('cors')
 
 // autorisation du client avec cors
 app.use(cors({
-    origin: ' 0.0.0.0',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
